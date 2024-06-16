3888f847d9ea54567e987b1f61c22033979057b2
rq-monakon-at-321899771753 <tnsykyou.ma@gmail.com>
2024-06-16 22:27:18 +0900

    会員登録機能実装

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,4 +1,6 @@
 class StaticpagesController < ApplicationController
+  # ログイン回避
+  skip_before_action :require_login, only: %i[top]
 
   def top;end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'staticpages#top'
+  resources :users, only: %i[new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
