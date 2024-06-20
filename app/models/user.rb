96a825f42222abee6211292cd75e9dd1e471d7d5
rq-monakon-at-321899771753 <tnsykyou.ma@gmail.com>
2024-06-21 07:58:31 +0900

    ログアウト時にcurrent_user nilのエラーを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
+
+  def own?(object)
+    object.user_id == id
+  end
 end
