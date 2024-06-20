e68745821c610efd121f0368d502915b94b941aa
rq-monakon-at-321899771753 <tnsykyou.ma@gmail.com>
2024-06-21 07:20:58 +0900

    nameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_18_032020) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_021635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,9 +30,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_18_032020) do
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
