3eb1c50edf2c7475ecc0c10ad00f0c0532dbd356
rq-monakon-at-321899771753 <tnsykyou.ma@gmail.com>
2024-08-05 22:53:18 +0900

    geocoder設定

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,10 @@ class Board < ApplicationRecord
   mount_uploader :board_image, BoardImageUploader
   belongs_to :user
 
+  # placeフィールドから緯度と経度を取得するためにgeocoderを設定
+  geocoded_by :place
+  after_validation :geocode, if: :place_changed?
+
   validates :title, presence: true, length: { maximum: 255 }
   validates :opinion, presence: true, length: { maximum: 65_535 }
   validates :background, presence: true, length: { maximum: 65_535 }
