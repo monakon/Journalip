93c415860a41754a4a52292b816a26a5a74c39f9
rq-monakon-at-321899771753 <tnsykyou.ma@gmail.com>
2024-09-12 13:10:01 +0900

    update:generateコマンド

--- config/application.rb
+++ config/application.rb
@@ -30,8 +30,13 @@ module Myapp
 
     config.generators do |g|
       g.helper false #helperを生成しない
-      g.test_framework false #testファイルを生成しない
-    # g.skip_routes true #ルーティングを生成しない
+      # g.test_framework false #testファイルを生成しない
+      g.test_framework :rspec, # ここから5行を追記
+        fixtures: false, # テストDBにレコードを作るfixtureの作成をスキップ(FactoryBotを使用するため)
+        view_specs: false, # ビューファイル用のスペックを作成しない
+        helper_specs: false, # ヘルパーファイル用のスペックを作成しない
+        routing_specs: false # routes.rb用のスペックファイル作成しない
+      # g.skip_routes true #ルーティングを生成しない
     end
 
     config.i18n.default_locale = :ja
