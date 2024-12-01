9d3bac07041695b89e896a3a119e601fbb9fcf90
rq-monakon-at-321899771753 <tnsykyou.ma@gmail.com>
2024-12-01 19:59:45 +0900

    update: bookmarkモデルとのアソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy
   has_many :authentications, dependent: :destroy
   accepts_nested_attributes_for :authentications
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmark_boards, through: :bookmarks, source: :board
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
